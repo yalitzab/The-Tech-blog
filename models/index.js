46cb1a413085604305f005b1087341c8fc89ca21
yalitzab <yalitzabueno@gmail.com>
2020-11-08 22:49:41 -0800

    initial files

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: "cascade"
+})
+
+module.exports = { User, Post, Comment };
